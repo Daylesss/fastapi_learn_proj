1bce458fbcb662cf021e279f5189eddb39fb24e7
Dayless <dayless@ubuntu.myguest.virtualbox.com>
2023-12-18 00:43:03 +0300

    unix update

--- learn/req.py
+++ learn/req.py
@@ -1,4 +1,4 @@
 import requests
 
-resp = requests.get('http://127.0.0.1:8000/protected-route', headers={"Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCIIkpXVCJ9.eyJzdWIiOiIzIiwiYXVkIjpbImZhc3RhcGktdXNlcnM6YXV0aCJdLCJleHAiOjE3MDI4MzU3ODR9.NSQzdRuObBasJHkPjljJVpyl9Mlt-aD11vskeYQumuE"})
+resp = requests.get('http://127.0.0.1:8000/protected-route', headers={"Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwiYXVkIjpbImZhc3RhcGktdXNlcnM6YXV0aCJdLCJleHAiOjE3MDI4NTI4NDR9.fUC-dmfR9Os1t_iXTVpEEiJKyUPMxZEAU-r99XfugKU"})
 print(resp.text)
